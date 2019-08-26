ddcf81f1b355c7272b4f5dcb0678632c9d9c032d
Zhongmien <zwang@datanetusa.com>
2019-08-26 17:45:17 -0400

    initial test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Calander start here</h1>
+        <input className="my-date-input"
+        type="text">
+          
+        </input>
       </header>
     </div>
   );
