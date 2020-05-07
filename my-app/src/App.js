70b64d1a541a219b946371c7615ff0ecd8269ff3
Zhongmian Wang <zwang@datanetusa.com>
2020-05-07 09:32:34 -0400

    Inital Testing Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,25 @@
-import React, {useState,useEffect} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  console.log("Start of apps");
+  let newTestValues = [1,2,3,4,5];
+  newTestValues.forEach((value)=>{
+    if(value === 2){
+      console.log("A == 2");
+    }else if(value === 3 ){
+      console.log("A == 3");
+    }else {
+      console.log("A != 3 ");
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Calander start here</h1>
         <input className="my-date-input"
-        type="text">
-          
+        type="text">    
         </input>
       </header>
     </div>
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,9 +1,69 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import App from './App';
+import _ from 'lodash';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe('App should run without error', () => {
+  test('App component renders without crashing', () => {
+    // user container as root
+    render(<App />, container);
+    
+    unmountComponentAtNode(container);
+  });
+
+  test('trancateStringByLength should return correct result', () => {
+    const testStrings = [
+        {
+            data:"1234567890123456789012345",
+            maxLength: 20,
+        },
+        {
+            data:"1234567890",
+            maxLength: 20,
+        },
+        {
+            data:"ABCDEFG",
+            maxLength: 20,
+        },
+        {
+          data:"",
+          maxLength: 20,
+      },
+      ];
+    let result = testStrings.map((item)=>{
+        return trancateStringByLength(item.data,item.maxLength)
+    });
+    let testValue = _.cloneDeep(result);
+    // this example display how to import library from NPM
+    console.log(testValue.length);
+    result.forEach((item,index)=>{
+        expect(item.length <= testStrings[index].maxLength).toBeTruthy();
+    });
+    
+  });
+  
+
+});
+
+function trancateStringByLength(inputString,maxLength = Number.MAX_SAFE_INTEGER) {
+    if(inputString != null && typeof inputString === "string"){
+        if(inputString.length > maxLength){
+          return inputString.substring(0,maxLength);
+        }
+    }
+  return inputString;
+}
